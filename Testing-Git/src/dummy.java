640880fae63fd96501a5d1f6553b81b56ba3cb89
deepaksharma143 <deepaksharma143@rediffmail.com>
2016-06-01 11:45:32 +0530

    Update dummy.java

--- Testing-Git/src/dummy.java
+++ Testing-Git/src/dummy.java
@@ -4,7 +4,7 @@ public class dummy {
 	public static void main(String[] args) {
 		System.out.println("TEsting");
 		System.out.println("My First Change");
-		System.out.println("My First Pull Try");
+		System.out.println("My First Pull Try1");
 	}
 
 }
